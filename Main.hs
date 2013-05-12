8bdb203e4c7b1b91c259e1cd2b982c12f619b7a1
Norton Jenkins <dominantshoe@gmail.com>
2013-05-11 22:36:51 -0400

    Added System.Directory support

--- Main.hs
+++ Main.hs
@@ -4,18 +4,20 @@ import ProjectSpecific
 import System.Process
 import System.Exit
 import System.Environment
-
+import System.Directory
 main = do
 	args <- getArgs
 	if ((length args) == 1) then do
-							boyFile <- openFile (at args 0) ReadMode
+							inUsrOne <- doesFileExist (at args 0)
+							boyFile <- openFile (if inUsrOne then (at args 0) else ("/usr/share/boy/boy1/" ++ (at args 0))) ReadMode
 							boyText <- hGetContents boyFile
 							(mapM putStrLn (lines boyText))
 							exitSuccess
 					 else
 							return ()
 	let (a:b:_) = take 2 args
-	realOptionBoyFile  <- openFile b ReadMode
+	inUsr <- doesFileExist b
+	realOptionBoyFile  <- openFile (if inUsr then b else ("/usr/share/boy/boy1/" ++ b)) ReadMode
 	realOptionBoyText <- hGetContents realOptionBoyFile
 	let response = case a of
 					"titles" -> (specTitles (lines realOptionBoyText))
